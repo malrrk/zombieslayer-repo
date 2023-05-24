8670e993a090b75d9547c65cb4704e10f35ea1a6
chris <christian.eckental@gmx.de>
2023-05-24 14:47:24 +0200

    map klasse von lena

--- zombieslayer/src/Map.java
+++ zombieslayer/src/Map.java
@@ -1,2 +1,20 @@
-public class Map {
+import ch.aplu.jgamegrid.*;
+import java.awt.event.KeyEvent;
+import java.awt.Color;
+public class map extends GameGrid {
+    private grafik frog;
+    public map() {
+        super(255, 255, 255, Color.red, "", false);
+        grafik frog = new grafik();
+        addActor(frog,new Location(0,0));
+        addKeyListener(frog);
+        show();
+        doRun();
+    }
+    public void mach(){
+
+
+    }
+    public  void turn4444(){
+    }
 }
