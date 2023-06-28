19ac8305011272bb38bb81d9a32eb3f1b86011cd
chris <christian.eckental@gmx.de>
2023-06-28 14:22:24 +0200

    map rand lila, zombies langsamer

--- desktop/src/src/Database.java
+++ desktop/src/src/Database.java
@@ -2,17 +2,27 @@ package src;
 import java.sql.*;
 public class Database{
     public static void main(String[] args) {
-        try
-        {
+        String url = "jdbc:mysql://localhost:3306/delftstackDB";
+        String username = "username";
+        String password = "password";
+
+
+        try {
             Class.forName("com.mysql.jdbc.Driver");
-            Connection con=DriverManager.getConnection(
-                    "jdbc:mysql://localhost:3306/delftstackDB","username","dbPassword");
-            Statement stmt=con.createStatement();
-            ResultSet rs=stmt.executeQuery("show databases;");
+            Connection con = DriverManager.getConnection(url,username,password);
+
+            Statement statement = con.createStatement();
+            ResultSet rs = statement.executeQuery("select MAX(highscore) from table");
             System.out.println("Connected");
+            while(rs.next()){
+                System.out.println(rs.getInt(1) + " Score:" + rs.getInt(2));
+            }
+
+            con.close();
+
         }
-        catch(Exception e)
-        {
+
+        catch(Exception e) {
             System.out.println(e);
         }
     }
